d7f33f0bd776196e3e9dcaffb54b8a38950c76a5
Alea2247 <alexandraachbergerova2007@gmail.com>
2025-02-07 15:58:48 +0100

    colors

--- src/configs/theme.ts
+++ src/configs/theme.ts
@@ -4,14 +4,14 @@ export const lightTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#1976d2", // Blue for light mode
+      main: "#DD7094", // Blue for light mode
     },
     secondary: {
-      main: "#ff4081",
+      main: "#C76787",
     },
     background: {
-      default: "#f5f5f5",
-      paper: "#ffffff",
+      default: "#F4D2DB",
+      paper: "#fdf0f0",
     },
     text: {
       primary: "#000000",
@@ -27,18 +27,18 @@ export const darkTheme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      main: "#d32f2f", // Red for dark mode
+      main: "#DD7094", // Red for dark mode
     },
     secondary: {
-      main: "#ff4081",
+      main: "#C76787",
     },
     background: {
-      default: "#121212",
-      paper: "#1e1e1e",
+      default: "#272727",
+      paper: "#553A43",
     },
     text: {
-      primary: "#ffffff",
-      secondary: "#bdbdbd",
+      primary: "#FDF0F0",
+      secondary: "#F4D2DB",
     },
   },
   typography: {
